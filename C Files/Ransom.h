f4fa31c3c5448a30c09e92f46885039a96d2fa14
nat-ten <nathan.tenn2002@gmail.com>
2025-09-27 17:15:15 -0400

    Delete C Files/Ransom.h

--- C Files/Ransom.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <windows.h>
-#include <wincrypt.h>
-#include <stdbool.h>
-#include "StringManips.h"
-
-void EncryptBytes(WCHAR filePath[], HKEY hKey);
-void DecryptBytes(WCHAR filePath[], HKEY hKey);
-WCHAR* RecursiveDirSearch(WCHAR folder[], WCHAR listOfFileExtensions[], HKEY hKey, bool decrypt);
